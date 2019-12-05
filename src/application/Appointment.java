816cb7dbd3fdea1f2e1dd40830f80ec95e288c2f
Richard Salvaty <richardsalvaty@hotmail.com>
2019-12-05 13:31:15 -0800

    Update Appointment.java
    added an arraylist

--- src/application/Appointment.java
+++ src/application/Appointment.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Appointment {
 	DayOfTheWeek day;
 	String time;
 	String appointment;
+	ArrayList<String> apptList = new ArrayList<>();
 	
 	public Appointment() {
 		//time = this.time;
@@ -12,9 +15,10 @@ public class Appointment {
 		//day = this.day;
 	}
 	
-	public Appointment(String t, String a, DayOfTheWeek d) {
+	public Appointment(String t, String a, DayOfTheWeek d, ArrayList<String> l) {
 		t = this.time;
 		a = this.appointment;
+		l = this.apptList;
 		d = this.day;
 	}
 
